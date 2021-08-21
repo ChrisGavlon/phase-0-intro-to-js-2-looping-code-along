a77b701d2b27afe9e5779e39ecdb8ac71d46f18b
Chris <ChrisGavlon@gmail.com>
2021-08-21 18:49:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, surprise ) {
+    let messages = [];
+
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+        console.log(messages[i])
+    }
+    return messages;
+}
+//writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) <-- this is to call the writeCards function
+function countDown(integer) {
+
+    while(integer >= 0){
+        console.log(integer--)
+        //debugger;
+    }
+}
+
+countDown(4);
